26c9700b65e0b849f758343c4ade187c25d54eeb
Daniel Cohen <dcohen@yotpo.com>
2022-02-16 21:53:02 +0200

    feat(Http#YotpoRetry):added

---
+++ Http/YotpoRetry.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Yotpo\Core\Http;
+
+use Magento\Framework\DataObject;
+use Yotpo\Core\Model\Config;
+use Magento\Framework\Exception\NoSuchEntityException;
+
+class YotpoRetry
+{
+    /**
+     * @var Config
+     */
+    protected $config;
+
+    const IS_SUCCESS_KEY = 'is_success';
+    const STATUS_KEY = 'status';
+
+    /**
+     * Processor constructor.
+     * @param Config $config
+     */
+    public function __construct(Config $config)
+    {
+        $this->config = $config;
+    }
+
+    /**
+     * Yotpo retry requests mechanism
+     * @param \Closure $request
+     * @return DataObject
+     */
+    public function executeRequest($request)
+    {
+        $attemptsLeftCount = $this->getMaxAttemptsAmount();
+
+        $attemptsLeftCount--;
+        $requestResult = $request();
+
+        while ($this->haveAdditionalAttempts($attemptsLeftCount)) {
+            if (!$requestResult->getData(self::IS_SUCCESS_KEY)) {
+                if ($this->config->isNetworkRetriableResponse($requestResult->getData(self::STATUS_KEY))) {
+                    // phpcs:ignore Magento2.Functions.DiscouragedFunction
+                    sleep(1);
+                    $attemptsLeftCount--;
+                    $requestResult = $request();
+                }
+            } else {
+                break;
+            }
+        }
+
+        return $requestResult;
+    }
+
+    /**
+     * @param int $attemptsLeftCount
+     * @return bool
+     */
+    private function haveAdditionalAttempts($attemptsLeftCount)
+    {
+        return $attemptsLeftCount > 0;
+    }
+
+    /**
+     * @return int
+     */
+    private function getMaxAttemptsAmount()
+    {
+        return $this->config->getYotpoRetryAttemptsAmount();
+    }
+}
