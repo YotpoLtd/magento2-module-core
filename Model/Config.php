e617fe40a90c2e9933ddc2afc364b661a13bd18a
LiorGingi <lgingi@yotpo.com>
2022-02-22 11:47:00 +0200

    feat(Config#buildYotpoEntityRequestUrl):added method for dynamically building request URL

--- Model/Config.php
+++ Model/Config.php
@@ -1,6 +1,7 @@
 <?php
 namespace Yotpo\Core\Model;
 
+use http\Exception\InvalidArgumentException;
 use Magento\Eav\Model\Entity;
 use Magento\Framework\App\ProductMetadataInterface;
 use Magento\Framework\Exception\LocalizedException;
@@ -29,6 +30,9 @@ class Config
     const YOTPO_RETRY_ATTEMPTS_AMOUNT = 3;
 
     const MODULE_NAME = 'Yotpo_Core';
+
+    const BOTH_PARENT_ENTITY_TYPE_AND_PARENT_ENTITY_ID_REQUIRED_EXCEPTION_MESSAGE = "Both parent type and parent ID are required";
+    const ENTITY_ID_REQUIRED_FOR_PATCH_EXCEPTION_MESSAGE = "Yotpo entity ID is required for PATCH requests";
 
     /**
      * API method types
@@ -673,5 +677,23 @@ class Config
     public function getYotpoRetryAttemptsAmount(): int
     {
         return self::YOTPO_RETRY_ATTEMPTS_AMOUNT;
+    }
+
+    public function buildYotpoEntityRequestUrl($yotpoEntityType, $requestMethod, $yotpoEntityId = null, $yotpoParentEntityType = null, $yotpoParentId = null) {
+        if (!($yotpoParentEntityType && $yotpoParentId)) {
+            throw new InvalidArgumentException(self::BOTH_PARENT_ENTITY_TYPE_AND_PARENT_ENTITY_ID_REQUIRED_EXCEPTION_MESSAGE);
+        }
+
+        $urlComponents = [$yotpoParentEntityType, $yotpoParentId, $yotpoEntityType];
+
+        if ($requestMethod == self::METHOD_PATCH) {
+            if (!$yotpoEntityId) {
+                throw new InvalidArgumentException(self::ENTITY_ID_REQUIRED_FOR_PATCH_EXCEPTION_MESSAGE);
+            }
+
+            $urlComponents[] = $yotpoEntityId;
+        }
+
+        return join('/', array_filter($urlComponents));
     }
 }
