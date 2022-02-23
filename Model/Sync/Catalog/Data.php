e11fcf5247c704ff8c01a31fea90c98e17376493
Daniel Cohen <dcohen@yotpo.com>
2022-02-23 20:21:47 +0200

    fix(CatalogProcessor#syncItems):create parent product for variant if missing

--- Model/Sync/Catalog/Data.php
+++ Model/Sync/Catalog/Data.php
@@ -327,7 +327,7 @@ class Data extends Main
      * @return array<string, string|array>
      * @throws NoSuchEntityException
      */
-    protected function attributeMapping(Product $item)
+    public function attributeMapping(Product $item)
     {
         $itemArray = [];
         $mapAttributes = $this->mappingAttributes;
