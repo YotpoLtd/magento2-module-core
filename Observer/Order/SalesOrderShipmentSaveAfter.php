bc02c3399b6feb728cbfdfb20508b33417f591e8
Nathan Day <nathan@windandkite.com>
2026-04-29 12:37:31 +0100

    [Fix] Remove Unused Imports

--- Observer/Order/SalesOrderShipmentSaveAfter.php
+++ Observer/Order/SalesOrderShipmentSaveAfter.php
@@ -2,16 +2,10 @@
 
 namespace Yotpo\Core\Observer\Order;
 
-use Magento\Framework\App\Area;
-use Magento\Framework\App\ResourceConnection;
 use Magento\Framework\Event\ObserverInterface;
 use Magento\Framework\Event\Observer;
 use Magento\Framework\Exception\LocalizedException;
 use Magento\Framework\Exception\NoSuchEntityException;
-use Yotpo\Core\Model\Sync\Orders\Processor as OrdersProcessor;
-use Yotpo\Core\Model\Config;
-use Magento\Checkout\Model\Session as CheckoutSession;
-use Magento\Framework\App\State as AppState;
 
 /**
  * Class SalesOrderShipmentSaveAfter
