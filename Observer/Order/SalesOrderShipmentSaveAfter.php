86240c5890ba65050d121e1e1ba159ac09b8b2dc
Nathan Day <nathan@windandkite.com>
2026-04-29 12:21:00 +0100

    [Feature] Sync Order after Shipment saved

---
+++ Observer/Order/SalesOrderShipmentSaveAfter.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Yotpo\Core\Observer\Order;
+
+use Magento\Framework\App\Area;
+use Magento\Framework\App\ResourceConnection;
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Event\Observer;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Exception\NoSuchEntityException;
+use Yotpo\Core\Model\Sync\Orders\Processor as OrdersProcessor;
+use Yotpo\Core\Model\Config;
+use Magento\Checkout\Model\Session as CheckoutSession;
+use Magento\Framework\App\State as AppState;
+
+/**
+ * Class SalesOrderShipmentSaveAfter
+ * Observer is called when shipment is created/updated
+ */
+class SalesOrderShipmentSaveAfter extends OrderMain implements ObserverInterface
+{
+
+    /**
+     * @var CheckoutSession
+     */
+    protected $checkoutSession;
+
+    /**
+     * @var AppState
+     */
+    protected $appState;
+
+    /**
+     * SalesOrderSaveAfter constructor.
+     * @param OrdersProcessor $ordersProcessor
+     * @param Config $yotpoConfig
+     * @param ResourceConnection $resourceConnection
+     * @param CheckoutSession $checkoutSession
+     * @param AppState $appState
+     */
+    public function __construct(
+        OrdersProcessor $ordersProcessor,
+        Config $yotpoConfig,
+        ResourceConnection $resourceConnection,
+        CheckoutSession $checkoutSession,
+        AppState $appState
+    ) {
+        $this->checkoutSession = $checkoutSession;
+        $this->appState = $appState;
+        parent::__construct($ordersProcessor, $yotpoConfig, $resourceConnection);
+    }
+
+    /**
+     * @param Observer $observer
+     * @throws LocalizedException
+     * @throws NoSuchEntityException
+     */
+    public function execute(Observer $observer)
+    {
+        $areaCode = $this->appState->getAreaCode();
+        $order = $observer->getEvent()->getShipment()->getOrder();
+
+        if ($areaCode !== Area::AREA_ADMINHTML && $order->getCustomerIsGuest()) {
+            $acceptsSmsMarketing = $this->checkoutSession->getYotpoSmsMarketing();
+            $this->ordersProcessor->updateOrderAttribute(
+                [$order->getEntityId()],
+                'yotpo_accepts_sms_marketing',
+                $acceptsSmsMarketing ?: 0
+            );
+        }
+
+        if ($order->getEntityId()) {
+            $this->processOrderSync($order);
+        }
+    }
+}
